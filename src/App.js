68567f0f274a157d4112610fc9d0875f2f1a7dad
realRJ10 <121657363+realRJ10@users.noreply.github.com>
2023-09-15 13:13:46 +0330

    added source

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        created by Mojdeh Khorsand Moghadam, open-sourced on{" "}
+        <a
+          href="https://github.com/realRJ10/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
