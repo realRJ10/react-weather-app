436147229f8c1a20838bed2887371d2a64858908
realRJ10 <121657363+realRJ10@users.noreply.github.com>
2023-09-15 15:54:27 +0330

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        created by Mojdeh Khorsand Moghadam, open-sourced on{" "}
-        <a
-          href="https://github.com/realRJ10/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          created by Mojdeh Khorsand Moghadam, open-sourced on{" "}
+          <a
+            href="https://github.com/realRJ10/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
