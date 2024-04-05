0ec4215d080fd8ea5672f0ed7c0220d75535d68c
realRJ10 <121657363+realRJ10@users.noreply.github.com>
2024-04-05 23:54:01 +0330

    added axios and make it work

--- src/Form.js
+++ src/Form.js
@@ -11,6 +11,7 @@ export default function Form() {
               className="form-control"
               placeholder="Type the city"
               id="input"
+              autoFocus="on"
             />
           </div>
         </div>
--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 import Form from "./Form";
 import City from "./City";
@@ -8,28 +9,42 @@ import Describe from "./Describe";
 import Forecast from "./Forecast";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <div className="container">
-        <Form />
-        <div className="row">
-          <City Name="Paris" />
-          <div className="col-6">
-            <Degree Temp={19} />
-            <Describe Weather="clear sky" />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+  function handleResponse(response) {
+    setTemperature(response.data.main.temp);
+    setReady(true);
+  }
+  if (ready) {
+    return (
+      <div className="Weather">
+        <div className="container">
+          <Form />
+          <div className="row">
+            <City Name="Paris" />
+            <div className="col-6">
+              <Degree Temp={temperature} />
+              <Describe Weather="clear sky" />
+            </div>
+            <div className="col-6">
+              <Preds
+                Humidity="51 %"
+                Wind="2.57 m/s"
+                Date="Sunday 17:22"
+                Feels="29 °C"
+              />
+            </div>
           </div>
-          <div className="col-6">
-            <Preds
-              Humidity="51 %"
-              Wind="2.57 m/s"
-              Date="Sunday 17:22"
-              Feels="29 °C"
-            />
-          </div>
+          <Forecast />
+          <script src="src/index.js"></script>
         </div>
-        <Forecast />
-        <script src="src/index.js"></script>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "f3009e4852fa0a079dab291dabf020c4";
+    let city = "Tehran";
+    let forecastUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    axios.get(forecastUrl).then(handleResponse);
+    return "loading...";
+  }
 }
